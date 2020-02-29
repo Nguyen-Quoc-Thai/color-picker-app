a56b8c72d24f93810feec6933e8607421b609125
Nguyễn Quốc Thái <43308712+Nguyen-Quoc-Thai@users.noreply.github.com>
2020-02-29 20:11:10 +0700

    Updates

---
+++ precache-manifest.80a06dc55316e71e97e47027c3697252.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37b383a9e69d2998f9ee321d3d2e35cc",
+    "url": "/color-picker-app/index.html"
+  },
+  {
+    "revision": "fb961a186cf20576be79",
+    "url": "/color-picker-app/static/css/main.908e660e.chunk.css"
+  },
+  {
+    "revision": "72084dbfbabfb993a5d5",
+    "url": "/color-picker-app/static/js/2.3645a998.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/color-picker-app/static/js/2.3645a998.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb961a186cf20576be79",
+    "url": "/color-picker-app/static/js/main.cc7a7cb9.chunk.js"
+  },
+  {
+    "revision": "7cdc7fa57a28be2bc4be",
+    "url": "/color-picker-app/static/js/runtime-main.d659bd01.js"
+  }
+]);
